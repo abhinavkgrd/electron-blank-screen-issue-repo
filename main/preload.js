bd011738681036408b366a7d5d82717c1445ade2
Abhinav <abhinavk.grd@gmail.com>
2023-12-28 08:47:07 +0530

    working example

--- main/preload.js
+++ main/preload.js
@@ -1,16 +1,11 @@
-/**
- * The preload script runs before. It has access to web APIs
- * as well as Electron's renderer process modules and some
- * polyfilled Node.js functions.
- *
- * https://www.electronjs.org/docs/latest/tutorial/sandbox
- */
-const { ipcRenderer, contextBridge } = require("electron");
+const { ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("electron", {
+const windowObject = window;
+
+windowObject["electron"] = {
   message: {
     send: (payload) => ipcRenderer.send("message", payload),
     on: (handler) => ipcRenderer.on("message", handler),
     off: (handler) => ipcRenderer.off("message", handler),
   },
-});
+};
